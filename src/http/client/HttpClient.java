5db878bf97efd6be96138c5cb2b565e9ffbef78d
Stijn Caerts <stijncaerts@gmail.com>
2017-03-16 15:25:48 +0100

    organize imports

--- src/http/client/HttpClient.java
+++ src/http/client/HttpClient.java
@@ -1,8 +1,6 @@
 package http.client;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
